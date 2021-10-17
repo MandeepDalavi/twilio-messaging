61341022e2c28438f86b0b57be0136e9b76ce9f9
MandeepDalavi <mandeepdalavi@gmail.com>
2021-10-17 22:30:02 +0530

    Setting up Environment Variables

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const MessagingResponse = require("twilio/lib/twiml/MessagingResponse");
 const app = express();
 const port = 3000;
 
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH;
+const twilioPhone = process.env.TWILIO_NUMBER;
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
