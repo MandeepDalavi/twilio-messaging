48a0025b1f6e0bddbd24912983ef8e2ae3583a7a
MandeepDalavi <mandeepdalavi@gmail.com>
2021-10-17 23:32:51 +0530

    Send Message Code Block

--- index.js
+++ index.js
@@ -7,12 +7,27 @@ const port = 3000;
 const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH;
 const twilioPhone = process.env.TWILIO_NUMBER;
+
+const client = new twilio(accountSid, authToken);
+
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/text", (req, res) => {
+  client.messages
+    .create({
+      body: "Hello, and Welcome to Local Hack Day",
+      to: process.env.USER_PHONE,
+      from: twilioPhone,
+    })
+    .then((message) => {
+      console.log(`Message sent: ${message.sid}`);
+      res.send(`Message sent: ${message.sid}`);
+    });
+});
 app.listen(port, () => {
   console.log(`Example app listening at "http://localhost:${port}`);
 });
